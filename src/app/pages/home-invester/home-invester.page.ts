0d0dfd4ea9ff4eb415e855e17339c06094a69263
Hariraagavanthar C R <hari.raghavandhar@myntra.com>
2019-10-04 21:17:08 +0530

    filters toggle added

--- src/app/pages/home-invester/home-invester.page.ts
+++ src/app/pages/home-invester/home-invester.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeInvesterPage implements OnInit {
 
+  filters: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleFilters(){
+    this.filters = !this.filters;
   }
 
 }
