2e68ac312b7ec2ed148c55c41e183296eacfdb8a
Hariraagavanthar C R <hari.raghavandhar@myntra.com>
2019-10-06 21:28:45 +0530

    made home admin

--- src/app/pages/home-admin/home-admin.page.ts
+++ src/app/pages/home-admin/home-admin.page.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAdminPage implements OnInit {
 
+  filters: boolean = true;
+
+  appliedFilters : Array<string> = []
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleFilters(){
+    this.filters = !this.filters;
+  }
+
+  toggleFilter(name : string){
+    let ind : number = this.appliedFilters.indexOf(name);
+
+    if(ind > -1)
+    {
+      //remove
+      this.appliedFilters.splice(ind, 1);
+
+    }else{
+      //add
+      this.appliedFilters.push(name)
+    }
+  }
+
+  checkFilter(name: string){
+    return this.appliedFilters.includes(name)
   }
 
 }
