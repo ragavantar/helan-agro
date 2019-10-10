b9f2c52a2c29f09782f76395a9d73553e3e19c4a
Hariraagavanthar C R <hari.raghavandhar@myntra.com>
2019-10-10 22:37:27 +0530

    removed generated default home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'registration-farmer', loadChildren: './pages/registration-farmer/registration-farmer.module#RegistrationFarmerPageModule' },
   { path: 'registration-farmer-equipments', loadChildren: './pages/registration-farmer-equipments/registration-farmer-equipments.module#RegistrationFarmerEquipmentsPageModule' },
