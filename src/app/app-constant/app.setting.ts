425ca34d5081115977d0e0c11e182024c887bfd1
Hariraagavanthar C R <hari.raghavandhar@myntra.com>
2019-10-31 19:38:41 +0530

    end point change

--- src/app/app-constant/app.setting.ts
+++ src/app/app-constant/app.setting.ts
@@ -1,3 +1,4 @@
 export class AppSettings {
-    public static API_ENDPOINT='http://10.50.5.74:8080/api/smarteqq-agro/v1';
+    // public static API_ENDPOINT='http://10.50.5.74:8080/api/smarteqq-agro/v1';
+    public static API_ENDPOINT='http://ec2-3-133-104-228.us-east-2.compute.amazonaws.com:8080/api/smarteqq-agro/v1';
 }
